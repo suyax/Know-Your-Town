5f93a16c2ad70d86773a85efba94e199d8b0b6e9
suya xu <tataxusuya@hotmail.com>
2016-03-09 10:04:50 -0800

    fix
    button style

--- client/js/style.js
+++ client/js/style.js
@@ -1,7 +1,7 @@
 var mq = window.matchMedia("(min-width:580px)");
 
 if (mq.matches) {
-  $('#trig').on('click', function(event) {
+  $('#switch-view').on('click', function(event) {
       event.preventDefault();
     $('#map').toggleClass('col-xs-12 col-xs-8');
     $('#left').toggleClass('col-xs-0 col-xs-4');
@@ -9,10 +9,10 @@ if (mq.matches) {
 
   });
 } else {
-  $('#trig').on('click', function(event) {
+  $('#switch-view').on('click', function(event) {
     event.preventDefault();
     $('#map').toggleClass('col-xs-12 col-xs-0');
     $('#left').toggleClass('col-xs-0 col-xs-12');
-    $('#trig').text() !== "Map View" ? $('#trig').text('Map View'): $('#trig').text('List View');
+    $('#switch-view').text() !== "Map View" ? $('#switch-view').text('Map View'): $('#switch-view').text('List View');
   });
 }
